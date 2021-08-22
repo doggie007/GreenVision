3d715ba69ae422db174c95db3425db91b611e1b1
James Tcheng <james.tcheng1234@gmail.com>
2021-08-22 17:18:05 +0800

    second commit finished

--- greenvision/views.py
+++ greenvision/views.py
@@ -1,3 +1,130 @@
-from django.shortcuts import render
-
+from django.http.response import HttpResponseRedirect
+from django.shortcuts import render, redirect
+from .forms import UploadFileForm
+from django.urls import reverse
 # Create your views here.
+import os
+import torch
+import torchvision
+from torch.utils.data import random_split
+import torchvision.models as models
+import torch.nn as nn
+import torch.nn.functional as F
+# from PIL import Image
+import torchvision.transforms as transforms
+from PIL import Image
+
+random_seed = 42
+torch.manual_seed(random_seed)
+
+def accuracy(outputs, labels):
+    _, preds = torch.max(outputs, dim=1)
+    return torch.tensor(torch.sum(preds == labels).item() / len(preds))
+
+class ImageClassificationBase(nn.Module):
+    def training_step(self, batch):
+        images, labels = batch 
+        out = self(images)                  # Generate predictions
+        loss = F.cross_entropy(out, labels) # Calculate loss
+        return loss
+    
+    def validation_step(self, batch):
+        images, labels = batch 
+        out = self(images)                    # Generate predictions
+        loss = F.cross_entropy(out, labels)   # Calculate loss
+        acc = accuracy(out, labels)           # Calculate accuracy
+        return {'val_loss': loss.detach(), 'val_acc': acc}
+        
+    def validation_epoch_end(self, outputs):
+        batch_losses = [x['val_loss'] for x in outputs]
+        epoch_loss = torch.stack(batch_losses).mean()   # Combine losses
+        batch_accs = [x['val_acc'] for x in outputs]
+        epoch_acc = torch.stack(batch_accs).mean()      # Combine accuracies
+        return {'val_loss': epoch_loss.item(), 'val_acc': epoch_acc.item()}
+    
+    def epoch_end(self, epoch, result):
+        print("Epoch {}: train_loss: {:.4f}, val_loss: {:.4f}, val_acc: {:.4f}".format(
+            epoch+1, result['train_loss'], result['val_loss'], result['val_acc']))
+
+class ResNet(ImageClassificationBase):
+    def __init__(self):
+        super().__init__()
+        # Use a pretrained model
+        self.network = models.resnet50(pretrained=True)
+        # Replace last layer
+        num_ftrs = self.network.fc.in_features
+        self.network.fc = nn.Linear(num_ftrs, 6)
+    
+    def forward(self, xb):
+        return torch.sigmoid(self.network(xb))
+def get_default_device():
+    """Pick GPU if available, else CPU"""
+    if torch.cuda.is_available():
+        return torch.device('cuda')
+    else:
+        return torch.device('cpu')
+    
+def to_device(data, device):
+    """Move tensor(s) to chosen device"""
+    if isinstance(data, (list,tuple)):
+        return [to_device(x, device) for x in data]
+    return data.to(device, non_blocking=True)
+
+class DeviceDataLoader():
+    """Wrap a dataloader to move data to a device"""
+    def __init__(self, dl, device):
+        self.dl = dl
+        self.device = device
+        
+    def __iter__(self):
+        """Yield a batch of data after moving it to device"""
+        for b in self.dl: 
+            yield to_device(b, self.device)
+
+    def __len__(self):
+        """Number of batches"""
+        return len(self.dl)
+
+model = ResNet()
+
+model.load_state_dict(torch.load("lol_state_dict.pb",map_location=torch.device('cpu')))
+model.eval()
+transform = transforms.Compose([transforms.Resize((256, 256)),transforms.ToTensor()])
+device = get_default_device()
+
+labels = {i:label for i,label in enumerate(['cardboard','glass','metal','paper','plastic','trash'])}
+
+def predict_image(img, model):
+    # Convert to a batch of 1
+    xb = to_device(img.unsqueeze(0), device)
+    # Get predictions from model
+    yb = model(xb)
+    # Pick index with highest probability
+    prob, preds  = torch.max(yb, dim=1)
+    return labels[int(preds)]
+
+
+def transform_image(img):
+    img_tr = transform(img)
+    return img_tr
+
+    
+
+
+def index(request):
+    if request.method == "POST":
+        form = UploadFileForm(request.POST,request.FILES)
+        if form.is_valid():
+            file = request.FILES['file']
+            img = Image.open(file)
+            img = transform_image(img)
+            pred = predict_image(img,model)
+            request.session['prediction'] = pred
+            return redirect("test")
+    else:
+        form = UploadFileForm()
+    context = {'form':form}
+    return render(request,'greenvision/index.html',context)
+
+def test(request):
+    return render(request,'greenvision/test.html')
